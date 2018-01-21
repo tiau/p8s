1f3b6d9655750bee9e6ecaf48159f45dc24d1ad5
tiau <isaac.chanin+gitemail@gmail.com>
2018-01-20 22:02:31 -0500

    Some defines updates

--- defines.h
+++ defines.h
@@ -7,28 +7,24 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 
-#define likely(x)	__builtin_expect((x),1)
-#define unlikely(x)	__builtin_expect((x),0)
-
-typedef uint_fast8_t  suit_t, card_t;
-typedef uint_fast16_t draw_t;
-
+/* These can safely be changed */
 #define CPP 11		// Cards dealt per player at the beginning of the game
 #define MINPLRS 2	// Minimum number of players
 #define MAXPLRS 4	// Maximum number of players
 
-/* N.B. Changing this will require some small reworks (e.g. initDeck, prbably others) */
+/* Changing these will require /significant/ reworks (poker rules + more...) */
 #define DECKLEN 52	// Deck size
-
-/* N.B. Changing this will require significant rework (poker rules...) */
 #define MAXCIP 5	// Maximum number of cards in a play
 
 /* Macros to deal with AIs returning their result as a packed 32-bit int.  We
- * pack the values like this as this makes our AI functions pure. */
+ * pack the values like this so our AI functions can be pure */
 #define ESPACK(A,B)	(A) = ((A) & 0xFFFF) | ((B) << 16)
 #define ESUPACK(A)	((A) >> 16)
 #define MPACK(A,B)	(A) = ((A) & 0xFFFF0000) | (B)
 #define MUPACK(A)	((A) & 0xFFFF)
+
+typedef uint_fast8_t  suit_t, card_t;
+typedef uint_fast16_t draw_t;
 
 enum suit {
 	Clubs,
@@ -45,7 +41,7 @@ enum gamestatus {
 };
 
 /* Players can only hold DECKLEN-2 cards since there must be at least one card
- * in the pile and at least one card in, at least, one opponent's hand. */
+ * in the pile and at least one card in >=1 opponent's hand */
 struct player {
 	card_t c[DECKLEN-2];
 	size_t n;
@@ -78,7 +74,7 @@ struct gamestate {
 	suit_t eightSuit;	// Suit to go on top of 8s, if magic
 	/* AI to invoke for each player slot */
 	uint_fast32_t (*ai[MAXPLRS])(const struct aistate* const restrict);
-	/* A bitmap of recent drawing events, 2 bits per turn */
+	/* A bitmap of recent drawing events, 2 bits (<=3 draws) per turn */
 	draw_t draws[MAXPLRS];
 };
 
@@ -119,5 +115,8 @@ inline static size_t max(const size_t x, const size_t y)
 {
 	return (x > y ? x : y);
 }
+
+#define likely(x)	__builtin_expect((x),1)
+#define unlikely(x)	__builtin_expect((x),0)
 
 #endif /* DEFINES_H */

23284ad94fa2bf09d896a69792509ac4666cc5e3
tiau <isaac.chanin+gitemail@gmail.com>
2018-01-26 21:06:13 -0500

    Parallelize GA

--- c.sh
+++ c.sh
@@ -3,7 +3,7 @@
 if [ -z "$2" ] ; then
 	o=p8
 else
-	p=$2
+	o=$2
 fi
 
 cc `cat $1 | awk '{ printf("-D"$1"="$2" "); }'` -march=native  -O3 -fomit-frame-pointer -ffast-math -DNDEBUG -fivopts -funsafe-loop-optimizations engine.c.o io.c.o mhash.c.o movegen.c.o p8.c.o plist.c.o poker.c.o ai/human.c.o ai/first.c ai/random.c.o ai/shedder.c.o ai/monte.c.o ai/judge.c.o ai/stacked.c.o ai/draw.c.o ai/cheat.c.o ai/mmcheat.c.o -std=gnu11 -D_GNU_SOURCE -lpthread -lrt -lm -s -o $o
--- evolve.sh
+++ evolve.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 games=10000
 scoretobeat=5100
 
@@ -36,11 +35,19 @@ function mutateandmerge() {
 }
 
 function newscore() {
-	dna=$1
-	./c.sh $dna
-	out="`./p8 -m14 -g$games | grep Player.0.won`"
-	score="`echo "$out" | awk '{ print $4 }'`"
-	echo "$score" >> scores/$dna
+	local dna=$1
+	local jf="`echo "$dna" | sed 's/^.*\/\([^\/]*\)$/\1/'`"
+	local nl="`cat scores/$dna 2>/dev/null | wc -l`"
+	if [ $nl -eq 0 -o `./randof 1 2 3` -eq 1 -a $nl -le 9 ] ; then
+		./c.sh $dna p8-$jf
+		local out="`./p8-$jf -m14 -g$games | grep Player.0.won`"
+		local score="`echo "$out" | awk '{ print $4 }'`"
+		echo "$score" >> scores/$dna
+		local out="`./p8-$jf -m14 -g$games | grep Player.0.won`"
+		local score="`echo "$out" | awk '{ print $4 }'`"
+		echo "$score" >> scores/$dna
+		rm p8-$jf
+	fi
 }
 
 function line {
@@ -53,18 +60,22 @@ if [ -n "$1" ] ; then
 		echo "Grew population, exiting now"
 		exit 0
 	elif [ -n "`echo "$1" | grep -i newscore`" ] ; then
-		if [ "`ps ax | grep evolve.sh | wc -l`" -gt 3 ] ; then
-			echo "Can't generate new scores while evolving"
-			exit 1
-		fi
-		echo "$Generating $(($#-1)) new scores"
+		echo "Generating $(($#-1)) new scores"
+		all=""
 		for i in ${*} ; do
 			if [ -f "$i" ] ; then
 				echo "Updating score for $i"
 				home="`echo "$i" | sed 's/^\(.*\/\)[^\/]*$/\1/'`"
 				jf="`echo "$i" | sed 's/^.*\/\([^\/]*\)$/\1/'`"
 				mv "$i" dna/ 2>/dev/null
-				newscore dna/$jf
+				all="$all dna/$jf"
+			fi
+		done
+		export -f newscore
+		export games
+		parallel --will-cite newscore ::: $all
+		for i in ${*} ; do
+			if [ -f "$i" ] ; then
 				mv dna/$jf $home 2>/dev/null
 			fi
 		done
@@ -89,15 +100,17 @@ while [ 1 ] ; do
 	line
 
 	scores=""
+	all=""
+	export -f newscore
+	export games
+	parallel --will-cite newscore ::: dna/*
 	for i in dna/* ; do
-		if [ -f scores/$i ] ; then
-			if [ `randof 1 2 3 4 5` -eq 1 ] ; then
-				newscore $i
-			fi
-		else
-			newscore $i
-		fi
 		score="`echo "($(cat scores/$i | tr '\n' '+' | sed 's/+$//'))/$(cat scores/$i | wc -l)" | bc`"
+		if [ -z "$score" ] ; then
+			echo "Looks like you're trying to quit (or something went wrong)"
+			rm -f p8-*
+			exit 1
+		fi
 		if [ $score -ge $scoretobeat ] ; then
 			scores="$scores $score"
 			if [ $score -ge $best ] ; then
